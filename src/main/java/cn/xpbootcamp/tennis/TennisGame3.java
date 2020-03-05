923ae9465175b2abd4984014165f369c295b092f
qian.guoliang <qngolg@126.com>
2020-03-05 14:52:37 +0800

    #001 rename

--- src/main/java/cn/xpbootcamp/tennis/TennisGame3.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGame3.java
@@ -14,16 +14,16 @@ public class TennisGame3 implements TennisGame {
 
     @Override
     public String getScore() {
-        String s;
+        String score;
         if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
             String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+            score = p[p1];
+            return (p1 == p2) ? score + "-All" : score + "-" + p[p2];
         } else {
             if (p1 == p2)
                 return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+            score = p1 > p2 ? p1N : p2N;
+            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + score : "Win for " + score;
         }
     }
 
