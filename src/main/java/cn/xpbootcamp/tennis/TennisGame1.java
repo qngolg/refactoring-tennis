6b0155fa771df7235de2ad12b363cfc88dddd262
qian.guoliang <qngolg@126.com>
2020-03-05 12:21:27 +0800

    #001 extract method refactoring long method

--- src/main/java/cn/xpbootcamp/tennis/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGame1.java
@@ -22,51 +22,76 @@ public class TennisGame1 implements TennisGame {
     @Override
     public String getScore() {
         String score = "";
-        int tempScore = 0;
-        if (player1.getScore() == player2.getScore()) {
-            switch (player1.getScore()) {
+        if (isDraw()) {
+            score = getDrawScore();
+        } else if (isMatchOver()) {
+            score = getMatchFinalScore();
+        } else {
+            score = getMatchScore(score);
+        }
+        return score;
+    }
+
+    private boolean isMatchOver() {
+        return player1.getScore() >= 4 || player2.getScore() >= 4;
+    }
+
+    private String getMatchScore(String score) {
+        int tempScore;
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) tempScore = player1.getScore();
+            else {
+                score += "-";
+                tempScore = player2.getScore();
+            }
+            switch (tempScore) {
                 case 0:
-                    score = "Love-All";
+                    score += "Love";
                     break;
                 case 1:
-                    score = "Fifteen-All";
+                    score += "Fifteen";
                     break;
                 case 2:
-                    score = "Thirty-All";
+                    score += "Thirty";
                     break;
-                default:
-                    score = "Deuce";
+                case 3:
+                    score += "Forty";
                     break;
+            }
+        }
+        return score;
+    }
 
-            }
-        } else if (player1.getScore() >= 4 || player2.getScore() >= 4) {
-            int minusResult = player1.getScore() - player2.getScore();
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
-        } else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = player1.getScore();
-                else {
-                    score += "-";
-                    tempScore = player2.getScore();
-                }
-                switch (tempScore) {
-                    case 0:
-                        score += "Love";
-                        break;
-                    case 1:
-                        score += "Fifteen";
-                        break;
-                    case 2:
-                        score += "Thirty";
-                        break;
-                    case 3:
-                        score += "Forty";
-                        break;
-                }
-            }
+    private String getMatchFinalScore() {
+        String score;
+        int minusResult = player1.getScore() - player2.getScore();
+        if (minusResult == 1) score = "Advantage player1";
+        else if (minusResult == -1) score = "Advantage player2";
+        else if (minusResult >= 2) score = "Win for player1";
+        else score = "Win for player2";
+        return score;
+    }
+
+    private boolean isDraw() {
+        return player1.getScore() == player2.getScore();
+    }
+
+    private String getDrawScore() {
+        String score;
+        switch (player1.getScore()) {
+            case 0:
+                score = "Love-All";
+                break;
+            case 1:
+                score = "Fifteen-All";
+                break;
+            case 2:
+                score = "Thirty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+
         }
         return score;
     }
