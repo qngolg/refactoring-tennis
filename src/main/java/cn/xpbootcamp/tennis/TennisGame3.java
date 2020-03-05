5ab7df1a008f70b8fade0897a6a28b83e0de81d2
qian.guoliang <qngolg@126.com>
2020-03-05 14:55:22 +0800

    #001 rename p1,p2 to point1,point2

--- src/main/java/cn/xpbootcamp/tennis/TennisGame3.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGame3.java
@@ -2,37 +2,37 @@ package cn.xpbootcamp.tennis;
 
 public class TennisGame3 implements TennisGame {
 
-    private int p2;
-    private int p1;
-    private String p1N;
-    private String p2N;
+    private int point2;
+    private int point1;
+    private String player1Name;
+    private String player2Name;
 
     public TennisGame3(String player1Name, String player2Name) {
-        this.p1N = player1Name;
-        this.p2N = player2Name;
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
     }
 
     @Override
     public String getScore() {
         String score;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
+        if (point1 < 4 && point2 < 4 && !(point1 + point2 == 6)) {
             String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-            score = p[p1];
-            return (p1 == p2) ? score + "-All" : score + "-" + p[p2];
+            score = p[point1];
+            return (point1 == point2) ? score + "-All" : score + "-" + p[point2];
         } else {
-            if (p1 == p2)
+            if (point1 == point2)
                 return "Deuce";
-            score = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + score : "Win for " + score;
+            score = point1 > point2 ? player1Name : player2Name;
+            return ((point1 - point2)*(point1 - point2) == 1) ? "Advantage " + score : "Win for " + score;
         }
     }
 
     @Override
     public void wonPoint(String playerName) {
         if ("player1".equals(playerName))
-            this.p1 += 1;
+            this.point1 += 1;
         else
-            this.p2 += 1;
+            this.point2 += 1;
 
     }
 
